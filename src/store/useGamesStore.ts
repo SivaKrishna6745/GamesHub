1f3fbbf284d778083c6fa75dd5bb4691d63f1f12
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-10-14 22:10:29 +0530

    chore: Update store variables as per alphabetical order

--- src/store/useGamesStore.ts
+++ src/store/useGamesStore.ts
@@ -2,37 +2,37 @@ import { create } from 'zustand';
 import type { gameGenre, gamePlatform, gameRes } from '../types';
 
 interface Store {
-    isDark: boolean;
-    activePlatform: string;
     activeGenre: string;
+    activePlatform: string;
     errorMessage: string;
     gamesGenres: gameGenre[];
-    gamesPlatforms: gamePlatform[];
     gamesList: gameRes[];
-    setIsDark: (value: boolean) => void;
-    setActivePlatform: (platform: string) => void;
+    gamesPlatforms: gamePlatform[];
+    isDark: boolean;
     setActiveGenre: (genre: string) => void;
+    setActivePlatform: (platform: string) => void;
     setErrorMessage: (error: string) => void;
     setGamesGenres: (genres: gameGenre[]) => void;
-    setGamesPlatforms: (platforms: gamePlatform[]) => void;
     setGamesList: (games: gameRes[]) => void;
+    setGamesPlatforms: (platforms: gamePlatform[]) => void;
+    setIsDark: (value: boolean) => void;
 }
 
 const useGamesStore = create<Store>((set) => ({
-    isDark: false,
     activePlatform: '',
     activeGenre: '',
     errorMessage: '',
     gamesGenres: [],
-    gamesPlatforms: [],
     gamesList: [],
-    setIsDark: (isDark: boolean) => set({ isDark: isDark }),
-    setActivePlatform: (platform: string) => set({ activePlatform: platform }),
+    gamesPlatforms: [],
+    isDark: false,
     setActiveGenre: (genre: string) => set({ activeGenre: genre }),
+    setActivePlatform: (platform: string) => set({ activePlatform: platform }),
     setErrorMessage: (error: string) => set({ errorMessage: error }),
     setGamesGenres: (genres: gameGenre[]) => set({ gamesGenres: genres }),
-    setGamesPlatforms: (platforms: gamePlatform[]) => set({ gamesPlatforms: platforms }),
     setGamesList: (games: gameRes[]) => set({ gamesList: games }),
+    setGamesPlatforms: (platforms: gamePlatform[]) => set({ gamesPlatforms: platforms }),
+    setIsDark: (isDark: boolean) => set({ isDark: isDark }),
 }));
 
 export default useGamesStore;
