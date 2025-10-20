8907fc0e8c80270d2fe2b196972b38927f8a0ada
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-10-20 13:54:50 +0530

    chore: name change in homepage URL and base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/games-hub/',
+    base: '/GamesHub/',
     plugins: [react(), tailwindcss()],
 });
